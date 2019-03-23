f2bfd84faa7424497af9caa44734172e41b569a5
Jason R Tibbetts <jason@tibbetts.net>
2019-03-23 17:26:02 -0400

    Initial Commit

---
+++ Medi8/Medi8.h
@@ -0,0 +1,19 @@
+//
+//  Medi8.h
+//  Medi8
+//
+//  Created by Jason R Tibbetts on 3/23/19.
+//  Copyright © 2019 Poikile Creations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Medi8.
+FOUNDATION_EXPORT double Medi8VersionNumber;
+
+//! Project version string for Medi8.
+FOUNDATION_EXPORT const unsigned char Medi8VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Medi8/PublicHeader.h>
+
+
---
+++ Medi8Tests/Medi8Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Medi8Tests.swift
+//  Medi8Tests
+//
+//  Created by Jason R Tibbetts on 3/23/19.
+//  Copyright © 2019 Poikile Creations. All rights reserved.
+//
+
+import XCTest
+@testable import Medi8
+
+class Medi8Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
