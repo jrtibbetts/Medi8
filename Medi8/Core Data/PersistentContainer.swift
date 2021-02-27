7999d3cd37da20079387412ff2b368c7ef66a17b
Jason R Tibbetts <jason@tibbetts.net>
2021-02-27 02:31:48 -0500

    #5: [refactor] Subclassed NSPersistentContainer and moved the container initialization code from FetchingTestBase into it.

---
+++ Medi8/Core Data/PersistentContainer.swift
@@ -0,0 +1,29 @@
+//  Copyright © 2021 Poikile Creations. All rights reserved.
+
+import Stylobate
+
+import CoreData
+
+/// An NSPersistentContainer for the Medi8 data model.
+open class PersistentContainer: NSPersistentContainer {
+
+    public init(inMemoryOnly: Bool = false) {
+        let model = NSManagedObjectModel.mergedModel(from: [Medi8.bundle])!
+        super.init(name: "Medi8", managedObjectModel: model)
+
+        if inMemoryOnly {
+            let inMemoryStoreDescription = NSPersistentStoreDescription()
+            inMemoryStoreDescription.type = NSInMemoryStoreType
+            persistentStoreDescriptions = [inMemoryStoreDescription]
+        }
+
+        loadPersistentStores { (storeDescription, error) in
+            if let error = error {
+                fatalError("Persistent store \(storeDescription.type) failed: \(error.localizedDescription)")
+            } else {
+                print("Successfully loaded \(storeDescription.type)")
+            }
+        }
+    }
+
+}
--- Medi8Tests/Core Data/FetchingTestBase.swift
+++ Medi8Tests/Core Data/FetchingTestBase.swift
@@ -6,26 +6,11 @@ import XCTest
 
 class FetchingTestBase: XCTestCase {
 
+    static var persistentContainer = PersistentContainer(inMemoryOnly: true)
+
     /// An `NSManagedObjectContext` backed by an in-memory store to make it
     /// suitable for unit testing. Based on an idea by
     /// https://www.andrewcbancroft.com/2015/01/13/unit-testing-model-layer-core-data-swift/
-    static var testingContext: NSManagedObjectContext = {
-        let model = NSManagedObjectModel.mergedModel(from: [Medi8.bundle])!
-        let container = NSPersistentContainer(name: "Medi8", managedObjectModel: model)
-        let inMemoryStoreDescription = NSPersistentStoreDescription()
-        inMemoryStoreDescription.type = NSInMemoryStoreType
-        container.persistentStoreDescriptions = [inMemoryStoreDescription]
-        container.loadPersistentStores { (storeDescription, error) in
-            if let error = error {
-                fatalError("Persistent store \(storeDescription.type) failed: \(error.localizedDescription)")
-            } else {
-                print("Successfully loaded \(storeDescription.type)")
-            }
-        }
-
-        print("Container context: \(container.viewContext)")
-
-        return container.viewContext
-    }()
+    static var testingContext = persistentContainer.viewContext
 
 }
