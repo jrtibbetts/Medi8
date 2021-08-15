4026b692f879f715504eb7f2d15d2ef60795b266
Jason R Tibbetts <jason@tibbetts.net>
2021-08-15 12:00:10 -0400

    #2: [add] Added NSSet.alphabetically and chronologically for MasterReleases.

--- Sources/Core Data/HasArtists.swift
+++ Sources/Core Data/HasArtists.swift
@@ -2,6 +2,7 @@
 
 import Foundation
 
+/// Implemented by elements that have an unordered set of `Artist`s.
 public protocol HasArtists {
 
     var artists: NSOrderedSet? { get }
@@ -10,6 +11,13 @@ public protocol HasArtists {
 
 public extension HasArtists {
 
+    /// The names of the artists, concatenated with a comma. The order of the
+    /// artists is the same as the order in which they were added to the
+    /// element.
+    ///
+    /// At some point, this should be expanded to allow custom separators, or
+    /// even mixed separators, such as "Elvis Costello and The Attractions
+    /// feat. Johnny Cash."
     var displayableArtistName: String {
         return artists?.compactMap { ($0 as? Artist)?.name }.joined(separator: ", ") ?? "Unknown"
     }
--- Sources/Core Data/MasterReleaseExtensions.swift
+++ Sources/Core Data/MasterReleaseExtensions.swift
@@ -22,3 +22,34 @@ public extension MasterRelease {
 extension MasterRelease: HasArtists, HasTitle {
     
 }
+
+public extension NSSet {
+
+    /// Get a list of ``MasterRelease``s, sorted alphabetically by their
+    /// ``MasterRelease/displayableTitle``s.
+    var alphabetically: [MasterRelease] {
+        self.allObjects
+            .compactMap { $0 as? MasterRelease }
+            .sorted { $0.displayableTitle < $1.displayableTitle }
+    }
+
+    /// Get an array of ``MasterRelease``s, sorted by their
+    /// ``MasterRelease/earliestReleaseDate``s.
+    var chronologically: [MasterRelease] {
+        self.allObjects
+            .compactMap { $0 as? MasterRelease }
+            .sorted { (leftRelease, rightRelease) in
+                switch (leftRelease.earliestReleaseDate, rightRelease.earliestReleaseDate) {
+                case (nil, nil):
+                    return leftRelease.displayableTitle < rightRelease.displayableTitle
+                case (nil, _):
+                    return false
+                case (_, nil):
+                    return true
+                default:
+                    return leftRelease.earliestReleaseDate! < rightRelease.earliestReleaseDate!
+                }
+            }
+    }
+
+}
